97759d200c78415e9ceaf89b7bb47c6f6a43d0ce
heffec <123609846+heffec@users.noreply.github.com>
2023-02-13 22:37:23 -0500

    Add files via upload

--- HW2/floating_point_tiling.c
+++ HW2/floating_point_tiling.c
@@ -5,10 +5,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <assert.h>
+#define SHORT_SIZE 16
 
 //*
 void SIMD_mult_float(float *a, float *b, float *c, int mSize, int tileSize)
 {
+    /*
     //remainder variable will add an extra 'chunk' if it's not equal to zero
     clock_t t = clock();
     //loop through the matrix by tile size
@@ -41,53 +43,54 @@ void SIMD_mult_float(float *a, float *b, float *c, int mSize, int tileSize)
     }
     t = clock()-t;
     printf("SIMD time of execution: (%f seconds).\n", ((float)t) / CLOCKS_PER_SEC);
+    */
 }
 //*/
 //*
 void SIMD_mult_short(const unsigned short *A, const unsigned short *B, unsigned short *C, int n)
 {
-
-    //with a matrix of short data types, the register can fit 16 values to operate on in parallel.
-    //This implementation divides each row and column length by 16 and performs the necessary
-    //operations to multiply the two randomly generated matrices. 
-
     __m256i a, b, c, d, sum;
     int chunks = n / SHORT_SIZE;
+    int remainder = n%SHORT_SIZE;
+    //remainder variable will add an extra 'chunk' if it's not equal to zero
+    short sSum;
     clock_t t = clock();
     // Matrix multiplication with AVX2 instructions
-        for (int i = 0; i < n; i++) {   //'i' represents matrix row
-            for (int chunk = 0; chunk < cchunkunks; chunk++) { //'h' represents each size-16 chunk that can fit in 1 row/column of the matrix
-                for (int j = 0; j < n; j++) {  //'j' represents matrix column
+        for (int i = 0; i < n; i++) {   //'i' represents each row of the matrix
+            for (int h = 0; h < chunks; h++) { //'h' represents each size-16 chunk that can fit in 1 row/column of the matrix
+                for (int j = 0; j < n; j++) {  //'j' represents each individual element in the matrix
                     sum = _mm256_setzero_si256();
-                    //a relates to the ith row of Matrix A
-                    //i*n is the first element in each chunk
-                    //chunk * SHORT_SIZE is the offset value
-                    //15-0 grabs each proceeding value in the matrix as it is part of the 'chunk'
-                    a = _mm256_set_epi16(A[i*n+chunk*SHORT_SIZE+15], A[i*n+chunk*SHORT_SIZE+14],
-                                          A[i*n+chunk*SHORT_SIZE+13], A[i*n+chunk*SHORT_SIZE+12],
-                                          A[i*n+chunk*SHORT_SIZE+11], A[i*n+chunk*SHORT_SIZE+10],
-                                          A[i*n+chunk*SHORT_SIZE+9], A[i*n+chunk*SHORT_SIZE+8],
-                                          A[i*n+chunk*SHORT_SIZE+7], A[i*n+chunk*SHORT_SIZE+6],
-                                          A[i*n+chunk*SHORT_SIZE+5], A[i*n+chunk*SHORT_SIZE+4],
-                                          A[i*n+chunk*SHORT_SIZE+3], A[i*n+chunk*SHORT_SIZE+2],
-                                          A[i*n+chunk*SHORT_SIZE+1], A[i*n+chunk*SHORT_SIZE+0]);
-                    //b relates to the jth row of matrix B
-                    //15*n = the first element in the 15th column of the matrix
-                    //n*SHORT_SIZE*chunk offsets the chunk downward by 16 in the matrix
-                    //j shifts the right as we increment
-                    b = _mm256_set_epi16(B[15*n+(n*SHORT_SIZE*chunk)+j], B[14*n+(n*SHORT_SIZE*chunk)+j],
-                                          B[13*n+(n*SHORT_SIZE*chunk)+j], B[12*n+(n*SHORT_SIZE*chunk)+j],
-                                          B[11*n+(n*SHORT_SIZE*chunk)+j], B[10*n+(n*SHORT_SIZE*chunk)+j],
-                                          B[9*n+(n*SHORT_SIZE*chunk)+j], B[8*n+(n*SHORT_SIZE*chunk)+j],
-                                          B[7*n+(n*SHORT_SIZE*chunk)+j], B[6*n+(n*SHORT_SIZE*chunk)+j],
-                                          B[5*n+(n*SHORT_SIZE*chunk)+j], B[4*n+(n*SHORT_SIZE*chunk)+j],
-                                          B[3*n+(n*SHORT_SIZE*chunk)+j], B[2*n+(n*SHORT_SIZE*chunk)+j],
-                                          B[1*n+(n*SHORT_SIZE*chunk)+j], B[0*n+(n*SHORT_SIZE*chunk)+j]);
-                    c = _mm256_add_epi32(_mm256_mulhi_epi16(a, b), _mm256_mullo_epi16(a, b)); //multiply the two matrices
+                    // a = row, b = column
+                    a = _mm256_set_epi16(A[i*n+h*SHORT_SIZE+15], A[i*n+h*SHORT_SIZE+14],
+                                          A[i*n+h*SHORT_SIZE+13], A[i*n+h*SHORT_SIZE+12],
+                                          A[i*n+h*SHORT_SIZE+11], A[i*n+h*SHORT_SIZE+10],
+                                          A[i*n+h*SHORT_SIZE+9], A[i*n+h*SHORT_SIZE+8],
+                                          A[i*n+h*SHORT_SIZE+7], A[i*n+h*SHORT_SIZE+6],
+                                          A[i*n+h*SHORT_SIZE+5], A[i*n+h*SHORT_SIZE+4],
+                                          A[i*n+h*SHORT_SIZE+3], A[i*n+h*SHORT_SIZE+2],
+                                          A[i*n+h*SHORT_SIZE+1], A[i*n+h*SHORT_SIZE+0]);
+                    b = _mm256_set_epi16(B[15*n+(n*SHORT_SIZE*h)+j], B[14*n+(n*SHORT_SIZE*h)+j],
+                                          B[13*n+(n*SHORT_SIZE*h)+j], B[12*n+(n*SHORT_SIZE*h)+j],
+                                          B[11*n+(n*SHORT_SIZE*h)+j], B[10*n+(n*SHORT_SIZE*h)+j],
+                                          B[9*n+(n*SHORT_SIZE*h)+j], B[8*n+(n*SHORT_SIZE*h)+j],
+                                          B[7*n+(n*SHORT_SIZE*h)+j], B[6*n+(n*SHORT_SIZE*h)+j],
+                                          B[5*n+(n*SHORT_SIZE*h)+j], B[4*n+(n*SHORT_SIZE*h)+j],
+                                          B[3*n+(n*SHORT_SIZE*h)+j], B[2*n+(n*SHORT_SIZE*h)+j],
+                                          B[1*n+(n*SHORT_SIZE*h)+j], B[0*n+(n*SHORT_SIZE*h)+j]);
+                    c = _mm256_add_epi32(_mm256_mulhi_epi16(a, b), _mm256_mullo_epi16(a, b)); 
                     sum += _mm256_add_epi32(sum, c);
                     
-                    //allocate the sum in its corresponding index in the resulting matrix
                     C[i*n+j] += _mm256_extract_epi16(sum, 15)+ _mm256_extract_epi16(sum, 14)+ _mm256_extract_epi16(sum, 13)+ _mm256_extract_epi16(sum, 12)+ _mm256_extract_epi16(sum, 11)+ _mm256_extract_epi16(sum, 10)+ _mm256_extract_epi16(sum, 9)+ _mm256_extract_epi16(sum, 8)+ _mm256_extract_epi16(sum, 7)+ _mm256_extract_epi16(sum, 6)+ _mm256_extract_epi16(sum, 5)+ _mm256_extract_epi16(sum, 4)+ _mm256_extract_epi16(sum, 3)+ _mm256_extract_epi16(sum, 2)+ _mm256_extract_epi16(sum, 1)+ _mm256_extract_epi16(sum, 0);
+                    //printf("C[%d] = %d\n", i*n+j, (_mm256_extract_epi16(sum, 15)+ _mm256_extract_epi16(sum, 14)+ _mm256_extract_epi16(sum, 13)+ _mm256_extract_epi16(sum, 12)+ _mm256_extract_epi16(sum, 11)+ _mm256_extract_epi16(sum, 10)+ _mm256_extract_epi16(sum, 9)+ _mm256_extract_epi16(sum, 8)+ _mm256_extract_epi16(sum, 7)+ _mm256_extract_epi16(sum, 6)+ _mm256_extract_epi16(sum, 5)+ _mm256_extract_epi16(sum, 4)+ _mm256_extract_epi16(sum, 3)+ _mm256_extract_epi16(sum, 2)+ _mm256_extract_epi16(sum, 1)+ _mm256_extract_epi16(sum, 0)));
+
+                    /*
+                    if ((i == 0 && h == 0) || (i == 0 && h == 1)) {
+                        printf("(%d,%d): A[%d-%d], B[%d-%d]   \t", i, h, i*n+h*SHORT_SIZE+0, i*n+h*SHORT_SIZE+15, 0*n+(n*SHORT_SIZE*h)+j, 15*n+(n*SHORT_SIZE*h)+j);
+                        printf("\n%d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d\n", _mm256_extract_epi16(a, 0), _mm256_extract_epi16(a, 1), _mm256_extract_epi16(a, 2), _mm256_extract_epi16(a, 3), _mm256_extract_epi16(a, 4), _mm256_extract_epi16(a, 5), _mm256_extract_epi16(a, 6), _mm256_extract_epi16(a, 7), _mm256_extract_epi16(a, 8), _mm256_extract_epi16(a, 9), _mm256_extract_epi16(a, 10), _mm256_extract_epi16(a, 11), _mm256_extract_epi16(a, 12), _mm256_extract_epi16(a, 13), _mm256_extract_epi16(a, 14), _mm256_extract_epi16(a, 15));
+                        printf("%d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d\n", _mm256_extract_epi16(b, 0), _mm256_extract_epi16(b, 1), _mm256_extract_epi16(b, 2), _mm256_extract_epi16(b, 3), _mm256_extract_epi16(b, 4), _mm256_extract_epi16(b, 5), _mm256_extract_epi16(b, 6), _mm256_extract_epi16(b, 7), _mm256_extract_epi16(b, 8), _mm256_extract_epi16(b, 9), _mm256_extract_epi16(b, 10), _mm256_extract_epi16(b, 11), _mm256_extract_epi16(b, 12), _mm256_extract_epi16(b, 13), _mm256_extract_epi16(b, 14), _mm256_extract_epi16(b, 15));
+                        printf("C[%d] = %d\n", i*n+j, (_mm256_extract_epi16(sum, 15)+ _mm256_extract_epi16(sum, 14)+ _mm256_extract_epi16(sum, 13)+ _mm256_extract_epi16(sum, 12)+ _mm256_extract_epi16(sum, 11)+ _mm256_extract_epi16(sum, 10)+ _mm256_extract_epi16(sum, 9)+ _mm256_extract_epi16(sum, 8)+ _mm256_extract_epi16(sum, 7)+ _mm256_extract_epi16(sum, 6)+ _mm256_extract_epi16(sum, 5)+ _mm256_extract_epi16(sum, 4)+ _mm256_extract_epi16(sum, 3)+ _mm256_extract_epi16(sum, 2)+ _mm256_extract_epi16(sum, 1)+ _mm256_extract_epi16(sum, 0)));
+                    }
+                    */
                 }
             }
         }
@@ -96,7 +99,7 @@ void SIMD_mult_short(const unsigned short *A, const unsigned short *B, unsigned 
 }
 //*/
 
-void trivial_mult_short(const short *A, const short *B, short *C, int n) {
+void trivial_mult_short(const unsigned short *A, const unsigned short *B, unsigned short *C, int n) {
     clock_t t = clock();
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
@@ -115,13 +118,12 @@ void trivial_mult_float(const float *A, const float *B, float *C, int n) {
         for (int j = 0; j < n; j++) {
             C[i*n + j] = 0;
             for (int k = 0; k < n; k++) {
-                //C[i*n + j] += A[i*n + k] * B[k*n + j];
                 C[i*n + j] += A[i*n + k] * B[k*n + j];
             }
         }
     }
     t = clock()-t;
-    printf("Naive time of execution: (%f seconds).\n", ((float)t) / CLOCKS_PER_SEC);
+    printf("SIMD time of execution: (%f seconds).\n", ((float)t) / CLOCKS_PER_SEC);
 }
 
 int main(int argc, char *argv[])
@@ -179,8 +181,8 @@ int main(int argc, char *argv[])
         //assigning random values to matrices
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                A[i*n+j] = rand();
-                B[i*n+j] = rand();
+                A[i*n+j] = rand()%256;
+                B[i*n+j] = rand()%256;
             }
         }
         
